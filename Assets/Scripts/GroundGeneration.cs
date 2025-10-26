a262afc9ffe3c99619f44d356a71aeb45d0a44a1
Milo Kesteloot <38082763+MiloKesteloot@users.noreply.github.com>
2025-10-25 21:02:58 -0700

    Added Background Art

---
+++ Assets/Scripts/GroundGeneration.cs
@@ -0,0 +1,118 @@
+using System;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+[RequireComponent(typeof(Grid))]
+public class GroundGeneration : MonoBehaviour
+{
+    public float spawnChance = 0.5f;
+    public GameObject[] prefabs;
+    List<GameObject> queue = new List<GameObject>();
+
+    private Camera camera;
+    private List<String> touchedSpots = new();
+
+    private Grid grid;
+    public int gridSize;
+
+    void Start()
+    {
+        camera = Camera.main;
+
+        grid = GetComponent<Grid>();
+        gridSize = (int) grid.cellSize.x;
+    }
+
+    void Update() {
+        // camera.pixelWidth/2, 
+        Vector3 worldPos = camera.ScreenToWorldPoint(new Vector3(-camera.pixelWidth / 2, -camera.pixelHeight / 2, camera.nearClipPlane));
+        Vector3 worldPos2 = camera.ScreenToWorldPoint(new Vector3(camera.pixelWidth / 2, camera.pixelHeight / 2, camera.nearClipPlane));
+
+        Vector3Int gridPos = grid.WorldToCell(GetBottomLeftAtY0());
+        Vector3Int gridPos2 = grid.WorldToCell(GetTopRightAtY0());
+
+        for (int x = 0; x < (gridPos2.x - gridPos.x) * 2 + 2; x++) {
+            for (int z = 1; z < (gridPos2.z - gridPos.z) * 2 + 2; z++) {
+                String str = "" + (gridPos.x + x) + "," + (gridPos.z + z);
+                if (touchedSpots.Contains(str)) continue;
+                touchedSpots.Add(str);
+                if (gridPos.x + x == 0 && gridPos.z + z == 0) continue;
+
+                if (UnityEngine.Random.Range(0, 1) > spawnChance) {
+                    // continue;
+                }
+
+                Vector3Int newPos = new(gridPos.x + x, 0, gridPos.z + z);
+                Vector3 backToWorldPos = grid.CellToWorld(newPos);
+                backToWorldPos.x += UnityEngine.Random.Range(-gridSize / 2f, gridSize / 2f);
+                backToWorldPos.z += UnityEngine.Random.Range(-gridSize / 2f, gridSize / 2f);
+
+                // float randomAngle = UnityEngine.Random.Range(0f, 360f);
+                // Quaternion randomRotation = Quaternion.Euler(0, 0, randomAngle);
+                Quaternion randomRotation = Quaternion.Euler(0, 0, 0);
+
+                Instantiate(getRandomObject(), backToWorldPos, randomRotation);
+            }
+        }
+    }
+
+    Vector3 GetBottomLeftAtY0() {
+        // Bottom-left corner of the screen
+        Vector3 screenPoint = new Vector3(0, 0, 0);
+
+        Ray ray = camera.ScreenPointToRay(screenPoint);
+
+        if (ray.direction.y == 0)
+            return ray.origin; // parallel, fallback
+
+        float t = -ray.origin.y / ray.direction.y;
+        return ray.origin + ray.direction * t;
+    }
+    
+    Vector3 GetTopRightAtY0() {
+        // Top-right corner of the screen
+        Vector3 screenPoint = new Vector3(camera.pixelWidth, camera.pixelHeight, 0);
+
+        Ray ray = camera.ScreenPointToRay(screenPoint);
+
+        if (ray.direction.y == 0)
+            return ray.origin;
+
+        float t = -ray.origin.y / ray.direction.y;
+        return ray.origin + ray.direction * t;
+    }
+
+    GameObject getRandomObject()
+    {
+        if (queue.Count == 0)
+        {
+            for (int i = 0; i < prefabs.Length; i++)
+            {
+                for (int ii = 0; ii < 3; ii++)
+                {
+                    queue.Add(prefabs[i]);
+                }
+            }
+            Shuffle(queue);
+        }
+        GameObject ret = queue[0];
+        queue.RemoveAt(0);
+        return ret;
+    }
+
+    private static System.Random rng = new System.Random();
+
+    public static void Shuffle(IList<GameObject> list)
+    {
+        int n = list.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = rng.Next(n + 1);
+            GameObject value = list[k];
+            list[k] = list[n];
+            list[n] = value;
+        }
+    }
+}
